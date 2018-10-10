21b5385ddc01a7fa859599304aa2c2bf49a1e939
Veniel Dorestave <veniel@noukod.com>
2018-10-10 15:29:24 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,13 +1,21 @@
+# class Appointment
+#   @@all = []
+#   attr_accessor :date, :doctor, :patient
+#   def initialize(date = "Monday, August 1st", doctor)
+#     @date = date
+#     @doctor = doctor
+#     # doctor.add_appointment(self)
+#     @@all << self
+#   end
+#   def self.all
+#   @@all
+#   end
+# end
 class Appointment
-  @@all = []
   attr_accessor :date, :doctor, :patient
-  def initialize(date = "Monday, August 1st", doctor)
+  def initialize(date, doctor)
     @date = date
     @doctor = doctor
-    # doctor.add_appointment(self)
-    @@all << self
-  end
-  def self.all
-  @@all
+    doctor.add_appointment(self)
   end
 end
