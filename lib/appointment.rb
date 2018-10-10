c00444335ddc423debd467793fa4ec96dc56f62a
Veniel Dorestave <veniel@noukod.com>
2018-10-10 15:22:13 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,9 +1,12 @@
 class Appointment
   @@all = []
   attr_accessor :date, :doctor, :patient
-  def initialize(date, doctor)
+  def initialize(date = "Monday, August 1st", doctor)
     @date = date
     @doctor = doctor
     doctor.add_appointment(self)
+  end
+  def self.all
+  @@all
   end
 end
